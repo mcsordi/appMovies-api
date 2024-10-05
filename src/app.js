5ab94575d97d381bf76453566efe04ba5fc7b620
mcsordi <matheuscarissordi@gmail.com>
2024-10-04 22:07:53 -0300

    express json added

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
-import express from "express";
+import express, { json } from "express";
 import routes from "./routes.js";
 import cors from "cors";
 const app = express();
 
+app.use(express.json());
 app.use(cors());
 app.use(routes);
 export default app;
