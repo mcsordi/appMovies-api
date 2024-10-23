c3c2eb588896eb2123675a6462f55c52cc070115
mcsordi <matheuscarissordi@gmail.com>
2024-10-23 01:27:40 -0300

    import and using dotEnv

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import routes from "./routes.js";
 import cors from "cors";
 
+
+
 const app = express();
 
 app.use(express.json());
--- src/app/database/connection.js
+++ src/app/database/connection.js
@@ -1,4 +1,5 @@
 import mysql from "mysql2";
+import "dotenv/config";
 
 
 const connection = mysql.createConnection(
